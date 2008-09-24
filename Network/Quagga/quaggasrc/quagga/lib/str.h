f4e10295e0ca783209cc3b608e1b8ca5678cb228
Dmitry Ivanov <ivanovd@1339srv.(none)>
2008-09-24 16:54:18 +0400

    Initial import of INET framework

---
+++ Network/Quagga/quaggasrc/quagga/lib/str.h
@@ -0,0 +1,24 @@
+/*
+ * $Id: str.h 922 2005-11-22 13:15:31Z andras $
+ */
+
+#ifndef _ZEBRA_STR_H
+#define _ZEBRA_STR_H
+
+#ifndef HAVE_SNPRINTF
+int snprintf(char *, size_t, const char *, ...);
+#endif
+
+#ifndef HAVE_VSNPRINTF
+#define vsnprintf(buf, size, format, args) vsprintf(buf, format, args)
+#endif
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif
+
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *, const char *, size_t);
+#endif
+
+#endif
